e514281c1bf570d03950f2e780596384ed91b516
dimi309 <dimi309@users.noreply.github.com>
2019-10-11 22:20:07 +0200

    Added the glfw window.

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,48 @@
 #include <stdio.h>
 #include <vector>
 #include <exception>
+#include <small3d/Renderer.hpp>
+
+using namespace small3d;
+
+bool downkey, leftkey, rightkey, upkey, esckey, enterkey;
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action,
+		 int mods) {
+  if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
+    downkey = true;
+  if (key == GLFW_KEY_UP && action == GLFW_PRESS)
+    upkey = true;
+  if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
+    leftkey = true;
+  if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
+    rightkey = true;
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    esckey = true;
+  if (key == GLFW_KEY_ENTER && action == GLFW_PRESS)
+    enterkey = true;
+  if (key == GLFW_KEY_DOWN && action == GLFW_RELEASE)
+    downkey = false;
+  if (key == GLFW_KEY_UP && action == GLFW_RELEASE)
+    upkey = false;
+  if (key == GLFW_KEY_LEFT && action == GLFW_RELEASE)
+    leftkey = false;
+  if (key == GLFW_KEY_RIGHT && action == GLFW_RELEASE)
+    rightkey = false;
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+    esckey = false;
+  if (key == GLFW_KEY_ENTER && action == GLFW_RELEASE)
+    enterkey = false;
+  
+}
 
 int posX, posY;
 
 std::vector<std::string> map;
 
 void render() {
+
+  
 
   for (int y = -1; y < 2; ++y) {
     if (posY + y >= 0 && posY + y < map.size()) {
@@ -26,6 +62,18 @@ void render() {
 
 
 int main(int argc, char** argv) {
+
+  Renderer *renderer = &Renderer::getInstance("Groom");
+
+  GLFWwindow *window = renderer->getWindow();
+
+  glfwSetKeyCallback(window, keyCallback);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+  
+  
   std::ifstream mapfile("resources/map.txt");
   if (mapfile.is_open()) {
     
