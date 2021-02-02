47c89257adba9b023910eda606058378bf29fd7b
dimi309 <dimi309@users.noreply.github.com>
2021-02-02 07:28:14 +0100

    Git correction

---
+++ chasethegoat/README.md
@@ -0,0 +1,28 @@
+Chase the Goat 3D
+=================
+
+*Requires small3d* - https://github.com/dimi309/small3d
+
+Chase the goat. Accelerate using the spacebar and navigate using the arrow keys.
+
+![screenshot](screenshot.png)
+
+Building
+--------
+
+Create a directory called *deps* inside the game project's root directory.
+Build small3d and copy the resulting *include*, *lib* and *shaders* directories from the small3d *build* directory to the *deps* directory created. Then from the game project's root directory, do the following:
+	
+	mkdir build
+    cd build
+    cmake ..
+    cmake --build .
+	
+On Windows, you need to execute `cmake .. -G"MinGW Makefiles"`, or with the preferred Visual Studio configuration (e.g. `cmake .. -G"Visual Studio 16 2019" -A x64`). Also if using Visual Studio, make sure to run build under the configuration that small3d was built with, e.g. `cmake --build . --config Debug`.
+
+In order to run the game, execute:
+	
+	cd bin
+	./chasethegoat3d (or chasethegoat3d.exe if you are on Windows)
+
+For building the game with an OpenGL build of small3d, just append `-DSMALL3D_OPENGL=ON` to the `cmake ..` command.
---
+++ frogremixed/src/Car.cpp
@@ -0,0 +1,49 @@
+/*
+ * Car.cpp
+ *
+ *  Created on: 2018/11/10
+ *      Author: Dimitri Kourkoulis
+ *     License: BSD 3-Clause License (see LICENSE file)
+ */
+
+#include "Car.hpp"
+#include <string>
+
+Car::Car() :
+  SceneObject("car", "resources/models/car/car.obj", 1,
+	      "resources/models/car/carBB.obj") {
+  wheel = Model("resources/models/car/wheel.obj");
+  this->colour = glm::vec4(0.0f, 0.0f, 1.0f, 1.0f);
+}
+
+Car::~Car() {
+  if (renderer) {
+    renderer->clearBuffers(wheel);
+    renderer->clearBuffers(static_cast<SceneObject &>(*this));
+  }
+}
+
+void Car::render(Renderer *renderer) {
+  this->renderer = renderer;
+  renderer->render(*this, colour);
+
+  renderer->render(this->wheel,
+		   glm::vec3(this->offset.x + (this->rotation.y > 0.0f ? 1.4f : 1.9f),
+			     this->offset.y,
+			     this->offset.z + 1.0f),
+		   this->rotation,
+		   glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
+
+  renderer->render(this->wheel,
+		   glm::vec3(this->offset.x - (this->rotation.y > 0.0f ? 1.9f : 1.4f),
+			     this->offset.y,
+			     this->offset.z + 1.0f),
+		   this->rotation,
+		   glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
+		   
+  
+}
+   
+void Car::move() {
+  this->offset.x += this->speed;
+}
