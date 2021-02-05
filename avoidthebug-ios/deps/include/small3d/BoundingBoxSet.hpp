bcf927af45fd2de831906aa7dbc94ce04e269b21
dimi309 <dimi309@users.noreply.github.com>
2021-02-05 21:46:01 +0100

    small3d update for ios

---
+++ avoidthebug-ios/deps/include/small3d/BoundingBoxSet.hpp
@@ -0,0 +1,142 @@
+/**
+ *  @file  BoundingBoxSet.hpp
+ *  @brief Bounding boxes for collision detection
+ *
+ *  Created on: 2014/10/19
+ *      Author: Dimitri Kourkoulis
+ *     License: BSD 3-Clause License (see LICENSE file)
+ *
+ */
+
+#pragma once
+
+#include <memory>
+#include "Logger.hpp"
+#include <vector>
+#include <glm/glm.hpp>
+#include "Model.hpp"
+
+namespace small3d {
+
+  /**
+   * @class BoundingBoxSet
+   * @brief Set of bounding boxes for a SceneObject, normally loaded from a 
+   *        Wavefront file, allowing for collision detection.
+   *        In order to create these in Blender for example (see blender.org), 
+   *        just place them in the preferred position over a model. Ideally, 
+   *        they should be aligned with the axes, (but note that small3d does 
+   *        more than just a simple axis-aligned bounding box collision 
+   *        detection).
+   *
+   *        Export the bounding boxes to a Wavefront file separately from the 
+   *        model. You can do this if you "save as" a new file after placing 
+   *        the boxes and deleting the original model. During export, only set 
+   *        the options Apply Modifiers, Include Edges (but not in newer 
+   *        versions of Blender, where it is not available), 
+   *        Objects as OBJ Objects and Keep Vertex Order. On the contrary to 
+   *        what is the case when exporting the Model itself, more than one 
+   *        bounding box objects can be exported to the same Wavefront file.
+   *
+   *        It is good to keep the default origin in Blender for the models 
+   *        as well as the bounding boxes. User-set origins are ignored by 
+   *        Blender when exporting Wavefront files. That can cause 
+   *        misalignments between bounding boxes and models, even if 
+   *        the origins of both have been properly set to a new position.
+   */
+
+  class BoundingBoxSet {
+  private:
+
+    int numBoxes;
+    void loadFromFile(std::string fileLocation);
+    void triangulate();
+    void calcExtremes();
+
+  public:
+
+    /**
+     * @brief Structure to hold the coordinates of the extremes of each box.
+     */
+    typedef struct extremes_ {
+      float minZ, maxZ, minX, maxX, minY, maxY;
+    } extremes;
+
+    /**
+     * @brief The extreme coordinates (max and min) of each box.
+     */
+    std::vector<extremes> boxExtremes;
+
+    /**
+     * @brief Get the number of boxes contained in the set.
+     */
+
+    int getNumBoxes() const;
+
+    /**
+     * @brief Constructor
+     * @param fileLocation Location of the Wavefront file containing the
+     *        bounding boxes
+     *
+     */
+
+    BoundingBoxSet(const std::string fileLocation = "");
+
+    /**
+     * @brief Destructor
+     */
+    ~BoundingBoxSet() = default;
+
+    /**
+     * @brief Vertex coordinates read from Wavefront .obj file
+     */
+
+    std::vector<std::vector<float> > vertices;
+
+    /**
+     * @brief Faces vertex indexes read from Wavefront .obj file (rectangles)
+     */
+
+    std::vector<std::vector<unsigned int> > facesVertexIndexes;
+
+    /**
+    * @brief Faces vertex indexes (triangles, for rendering)
+    */
+    std::vector<std::vector<unsigned int> > facesVertexIndexesTriangulated;
+
+    /**
+     * @brief Check if a point is inside any of the boxes.
+     * @param point        The point (as a vector)
+     * @param thisOffset   The offset (location) of the box set
+     * @param thisRotation The rotation of the box set
+     * @return True the point is inside a box, False if not.
+     */
+
+    bool contains(const glm::vec3 point, const glm::vec3 thisOffset,
+      const glm::vec3 thisRotation) const;
+
+    /**
+     * @brief Check any of the corners of another set of bounding boxes 
+     *        is inside any of the boxes of this set.
+     * @param otherBoxSet   The other box set
+     * @param thisOffset    The offset (location) of this box set
+     * @param thisRotation  The rotation of this box set
+     * @param otherOffset   The offset (location) of the other box set
+     * @param otherRotation The rotation of the other box set
+     * @return True if a corner of the other bounding box set is contained in 
+     *         this set, False otherwise.
+     */
+
+    bool containsCorners(const BoundingBoxSet otherBoxSet,
+      const glm::vec3 thisOffset,
+      const glm::vec3 thisRotation,
+      const glm::vec3 otherOffset,
+      const glm::vec3 otherRotation) const;
+
+    /**
+     * @brief Get the bounding boxes in a set of Models that can be rendered
+     * @return The set of bounding boxes as Models
+     */
+    std::vector<Model> getModels();
+
+  };
+}
---
+++ avoidthebug-ios/deps/include/small3d/SceneObject.hpp
@@ -0,0 +1,188 @@
+/**
+ * @file  SceneObject.hpp
+ * @brief An object on the small3d scene
+ *
+ *  Created on: 2014/10/19
+ *      Author: Dimitri Kourkoulis
+ *     License: BSD 3-Clause License (see LICENSE file)
+ */
+
+#pragma once
+
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <stdexcept>
+#include <memory>
+
+#include "Model.hpp"
+#include "Logger.hpp"
+#include "Image.hpp"
+#include "BoundingBoxSet.hpp"
+#include <glm/glm.hpp>
+
+namespace small3d
+{
+  /**
+   * @class SceneObject
+   *
+   * @brief An object that appears on the 3D scene. It is made up of one or more
+   *        models (the latter for animation), together with information about
+   *        positioning and rotation and collision detection functionality.
+   *        Models can be loaded from a Wavefront file.
+   *        Such a file can be exported from Blender for example (see blender.org).
+   *        From its menu, select File > Export > Wavefront (.obj). Then from the
+   *        "Export OBJ" menu, only select "Write Normals", "Triangulate Faces" 
+   *        and "Keep Vertex Order".
+   *        For exporting animations to a series of Wavefront files in a way 
+   *        that will allow this class to load them, also select "Animation" and 
+   *        "Apply Modifiers".*
+   *
+   */
+
+  class SceneObject
+  {
+  private:
+    
+    bool animating;
+    int frameDelay;
+    int currentFrame;
+    int framesWaited;
+    int numFrames;
+    std::string name;
+
+  public:
+    /**
+     * @brief The models making up the object's animation. Do not manipulate
+     *        directly.
+     */
+    std::vector<Model> models;
+
+    /**
+     * @brief Constructor
+     *
+     * @param name      The name of the object
+     * @param modelPath The path to the file containing the object's model.
+     *                  When animating (also see numFrames parameter) it is
+     *                  assumed that multiple files will be loaded, the name
+     *                  of which contains a 6 digit suffix (after an 
+     *                  underscore) representing the animation sequence, and
+     *                  terminates with the .obj extension. This is also the
+     *                  format used by Blender when exporting animations to
+     *                  Wavefront files. When loading multiple models for 
+     *                  animation do not enter a full filename. Skip the
+     *                  suffix and the extension. For example an animation
+     *                  sequence made up of files named frog_000001.obj,
+     *                  frog_000002.obj etc. should be loaded using the 
+     *                  parameter "directory/frog" here.
+     *
+     * @param numFrames The number of frames, if the object is animated. A
+     *                  single animation sequence is supported per object and
+     *                  the first frame is considered to be the non-moving 
+     *                  state.
+     *
+     * @param boundingBoxSetPath The path to the file containing the object's
+     *                           bounding box set. If no such path is given, the
+     *                           object cannot be checked for collision
+     *                           detection.
+     * @param startFrameIndex The index number in the filename of the first file
+     *                        of the animation sequence. The default value is 1.
+     *                        If not loading an animation sequence, this parameter
+     *                        is ignored.
+     */
+    SceneObject(const std::string name, const std::string modelPath,
+		const int numFrames = 1,
+		const std::string boundingBoxSetPath = "", const int startFrameIndex = 1);
+
+    /**
+     * @brief Destructor
+     */
+    ~SceneObject() = default;
+
+    /**
+     * @brief Get the object's model
+     * @return The object's model
+     */
+    Model& getModel() ;
+
+    /**
+     * @brief Is this an animated or a static object (is it associated with more than
+     *        one frames/models)?
+     * @return True if animated, False otherwise.
+     */
+    bool isAnimated() const;
+
+    /**
+     * @brief Get the name of the object
+     * @return The name of the object
+     */
+    const std::string getName() const;
+
+    /**
+     * Offset (position) of the object
+     */
+    glm::vec3 offset;
+
+    /**
+     * Rotation of the object (on x, y, z axes respectively)
+     */
+    glm::vec3 rotation;
+
+    /**
+     * @brief Start animating the object
+     */
+    void startAnimating();
+
+    /**
+     * @brief Stop animating the object
+     */
+    void stopAnimating();
+
+    /**
+     * @brief Reset the animation sequence (go to the first frame)
+     */
+    void resetAnimation();
+
+    /**
+     * @brief Set the animation speed
+     * @param delay The delay between each animation frame, expressed in number
+     *              of game frames
+     */
+    void setFrameDelay(const int delay);
+
+    /**
+     * @brief Process animation (progress current frame if necessary)
+     */
+    void animate();
+
+    /**
+     * @brief The bounding boxes for the object, used for collision detection.
+     */
+
+    BoundingBoxSet boundingBoxSet;
+
+    /**
+     * @brief  Check if the bounding boxes of this object contain 
+     *         a given point.
+     * @param  point The point
+     * @return True if the point is contained in the bounding boxes
+     *         of the object, False otherwise.
+     */
+
+    bool contains(const glm::vec3 point) const;
+
+    /**
+     *
+     * @brief	 Check if the bounding boxes of this object contain 
+     *         a corner of the bounding boxes of another object.
+     * @param	 otherObject The other object.
+     * @return True if the bounding boxes of this object contain
+     *         a corner of the bounding boxes of another object,
+     *         False otherwise.
+     */
+
+    bool containsCorners(SceneObject otherObject) const;
+
+  };
+  
+}
